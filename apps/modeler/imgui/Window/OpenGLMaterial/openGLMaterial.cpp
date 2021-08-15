6c69af798291c1e51c341ca82d0d9b203973b468
Janis Joderi Shoferi <janis.joderishoferi@campus.tu-berlin.de>
2021-08-15 19:02:20 +0200

    Put Shader Code Modal into its own file

--- apps/modeler/imgui/Window/OpenGLMaterial/openGLMaterial.cpp
+++ apps/modeler/imgui/Window/OpenGLMaterial/openGLMaterial.cpp
@@ -10,7 +10,6 @@
 #include <Raytracing/Components/Material/raytracingMaterial.h>
 #include <filesystem>
 #include <glad/glad.h>
-#include <misc/cpp/imgui_stdlib.h>
 
 namespace fs = std::filesystem;
 
@@ -19,35 +18,9 @@ extern bool dragging;
 UICreation::MaterialComponentWindow::MaterialComponentWindow(int &currentEntity,
                                                              Engine::Registry &registry,
                                                              Engine::Util::OpenGLTextureIndex &textureIndex)
-    : ComponentWindow{"Material", currentEntity, registry}, m_textureIndex{textureIndex}
+    : ComponentWindow{"Material", currentEntity, registry}, m_textureIndex{textureIndex}, m_shaderWindow{currentEntity,
+                                                                                                         registry}
 {
-}
-
-void UICreation::MaterialComponentWindow::onEntityChange(int oldEntity)
-{
-    ComponentWindow::onEntityChange(oldEntity);
-
-    checkShaderChange();
-}
-
-void UICreation::MaterialComponentWindow::checkUpdates()
-{
-    ComponentWindow::checkUpdates();
-
-    checkShaderChange();
-}
-
-void UICreation::MaterialComponentWindow::checkShaderChange()
-{
-    auto currentShader{m_registry.getComponent<Engine::OpenGLShaderComponent>(m_currentEntity)};
-    if (currentShader != m_shader)
-    {
-        m_shader = currentShader;
-        if (m_shader)
-        {
-            loadShaders(m_shader->getShaders());
-        }
-    }
 }
 
 void UICreation::MaterialComponentWindow::render()
@@ -67,158 +40,6 @@ bool isImage(const fs::path &path)
     return extension == ".jpg" || extension == ".jpeg" || extension == ".png";
 }
 
-bool isShaderFile(const fs::path &path)
-{
-    try
-    {
-        Engine::shaderPathToType(path);
-    }
-    catch (Engine::ShaderException &err)
-    {
-        return false;
-    }
-
-    return true;
-}
-
-bool isShaderDirectory(const fs::path &path)
-{
-    if (!fs::is_directory(path))
-    {
-        return false;
-    }
-
-    for (const auto &entry : fs::directory_iterator(path.c_str()))
-    {
-        if (!isShaderFile(entry))
-        {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-void UICreation::MaterialComponentWindow::loadShader(const Engine::OpenGLShader &shader)
-{
-    const char *shaderType{};
-
-    switch (shader.m_type)
-    {
-    case GL_VERTEX_SHADER:
-        shaderType = "Vertex Shader";
-        break;
-    case GL_FRAGMENT_SHADER:
-        shaderType = "Fragment Shader";
-        break;
-    case GL_GEOMETRY_SHADER:
-        shaderType = "Geometry Shader";
-        break;
-    case GL_TESS_EVALUATION_SHADER:
-        shaderType = "Tesselation Evaluation Shader";
-        break;
-    case GL_TESS_CONTROL_SHADER:
-        shaderType = "Tesselation Control Shader";
-        break;
-    case GL_COMPUTE_SHADER:
-        shaderType = "Compute Shader";
-        break;
-    }
-
-    // if there isn't a shader of this type already => just add
-    auto it = std::find_if(
-        m_shaders.begin(), m_shaders.end(), [&shader](Engine::OpenGLShader &el) { return el.m_type == shader.m_type; });
-    if (it == m_shaders.end())
-    {
-        m_shaders.push_back(shader);
-        m_shaderTypes.push_back(shaderType);
-    }
-    else
-    {
-        // there is already a shader of this type != replace source
-        it->m_source = shader.m_source;
-    }
-}
-
-void UICreation::MaterialComponentWindow::loadShaders(const std::vector<Engine::OpenGLShader> &shaders)
-{
-    m_shaders.clear();
-    m_shaderTypes.clear();
-    m_currentShader = 0;
-
-    for (const Engine::OpenGLShader &shader : shaders)
-    {
-        loadShader(shader);
-    }
-}
-
-void UICreation::MaterialComponentWindow::drawShaderEditModal(
-    unsigned int entity, Engine::Registry &registry, const std::shared_ptr<Engine::OpenGLShaderComponent> &shader)
-{
-    if (!m_modalOpen)
-    {
-        return;
-    }
-
-    if (ImGui::Begin("Shader Editor", &m_modalOpen))
-    {
-        ImGui::Combo("##shader_type", &m_currentShader, m_shaderTypes.data(), m_shaderTypes.size());
-
-        ImVec2 pos{ImGui::GetWindowContentRegionMin()};
-        ImVec2 end{ImGui::GetWindowContentRegionMax()};
-        ImVec2 textSize{pos.x + end.x, pos.y + end.y - 100};
-
-        ImGui::InputTextMultiline("##shader_source_input", &m_shaders[m_currentShader].m_source, textSize);
-
-        if (auto path = createImGuiHighlightedDropTarget<fs::path>(
-                "system_path_payload",
-                [](const fs::path &path) { return (isShaderFile(path) || isShaderDirectory(path)); }))
-        {
-            if (fs::is_directory(*path))
-            {
-                loadShaders(Engine::loadShaders(path->c_str()));
-            }
-            else
-            {
-                loadShader(Engine::loadShader(*path));
-            }
-        }
-
-        if (ImGui::Button("Close", ImVec2(120, 0)))
-        {
-            m_modalOpen = 0;
-        }
-        ImGui::SameLine();
-        ImGui::Button("Export##multi_shader", ImVec2(120, 0));
-
-        if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None))
-        {
-            ImGui::SetDragDropPayload("shaders_payload", &m_shaders, sizeof(std::vector<Engine::OpenGLShader>));
-
-            ImGui::Text("Shaders");
-            ImGui::EndDragDropSource();
-        }
-
-        ImGui::SameLine();
-        if (ImGui::Button("Use", ImVec2(120, 0)))
-        {
-            try
-            {
-                shader->updateShaders(m_shaders);
-                registry.updated<Engine::OpenGLShaderComponent>(entity);
-            }
-            catch (Engine::ShaderException &err)
-            {
-                errorMessage = err.what();
-                ImGui::OpenPopup("Error Info");
-            }
-        }
-
-        UIUtil::drawErrorModal(errorMessage);
-    }
-    ImGui::End();
-}
-
 void UICreation::MaterialComponentWindow::main()
 {
 
@@ -227,7 +48,7 @@ void UICreation::MaterialComponentWindow::main()
         ImGui::Text("OpenGL Shader");
         ImGui::SameLine();
         ImGui::Button("Drag##OpenGLShader");
-        createImGuiComponentDragSource<Engine::OpenGLShaderComponent>(m_shader);
+        createImGuiComponentDragSource<Engine::OpenGLShaderComponent>(m_shaderWindow.getShader());
         if (!m_registry.hasComponent<Engine::RenderComponent>(m_currentEntity))
         {
             ImGui::SameLine();
@@ -239,14 +60,9 @@ void UICreation::MaterialComponentWindow::main()
 
         if (ImGui::Button("Edit Shaders"))
         {
-            loadShaders(m_shader->getShaders());
-            m_modalOpen = 1;
+            m_shaderWindow.open();
         }
-        drawShaderEditModal(m_selectedEntity, m_registry, m_shader);
-        const char *types[2]{"Points\0", "Triangles\0"};
-        static int primitive_type_current = 1;
-        const char *comboLabel = types[primitive_type_current];
-        static int primitive_type = GL_TRIANGLES;
+        m_shaderWindow.render();
 
         bool isRendered = m_registry.hasComponent<Engine::RenderComponent>(selectedEntity);
         ImGui::SameLine();
--- apps/modeler/imgui/Window/Templates/componentWindow.h
+++ apps/modeler/imgui/Window/Templates/componentWindow.h
@@ -2,70 +2,80 @@
 #define APPS_MODELER_IMGUI_COMPONENT_WINDOW_CLASS
 
 #include "imguiWindow.h"
-#include <string>
 #include <Core/ECS/registry.h>
+#include <string>
 
-namespace UICreation {
+namespace UICreation
+{
 
-class ComponentWindow : public ImGuiWindow {
+class ComponentWindow : public ImGuiWindow
+{
 
 public:
-  ComponentWindow() = delete;
-  ComponentWindow(const std::string &name, int &currentEntity, Engine::Registry &registry);
+    ComponentWindow() = delete;
+    ComponentWindow(const std::string &name, int &currentEntity, Engine::Registry &registry);
 
 protected:
-  int &m_selectedEntity;
-  int m_currentEntity{-1};
-  Engine::Registry &m_registry;
+    int &m_selectedEntity;
+    int m_currentEntity{-1};
+    Engine::Registry &m_registry;
 
-  // extends update checks by tracking the selected entity
-  virtual void checkUpdates() override;
-  // allows custom handlers for entity changes in deriving classes (responsible for updating the current entity)
-  virtual void onEntityChange(int oldEntity);
+    // extends update checks by tracking the selected entity
+    virtual void checkUpdates() override;
+    // allows custom handlers for entity changes in deriving classes (responsible for updating the current entity)
+    virtual void onEntityChange(int oldEntity);
 };
 
 template <typename CompType>
-class TemplatedComponentWindow : public ComponentWindow {
+class TemplatedComponentWindow : public ComponentWindow
+{
 
 public:
-  TemplatedComponentWindow() = delete;
-  TemplatedComponentWindow(const std::string &name, int& currentEntity, Engine::Registry &registry)
-    : ComponentWindow{name, currentEntity, registry}
-  {}
+    TemplatedComponentWindow() = delete;
+    TemplatedComponentWindow(const std::string &name, int &currentEntity, Engine::Registry &registry)
+        : ComponentWindow{name, currentEntity, registry}
+    {
+    }
 
 protected:
-  std::shared_ptr<CompType> m_component;
+    std::shared_ptr<CompType> m_component;
 
-  virtual void render() override {
-    if (m_registry.hasComponent<CompType>(m_selectedEntity)) {
-      ComponentWindow::render();
+    virtual void render() override
+    {
+        if (m_registry.hasComponent<CompType>(m_selectedEntity))
+        {
+            ComponentWindow::render();
+        }
     }
-  }
 
-  virtual void onEntityChange(int oldEntity) override {
-    ComponentWindow::onEntityChange(oldEntity);
+    virtual void onEntityChange(int oldEntity) override
+    {
+        ComponentWindow::onEntityChange(oldEntity);
 
-    checkComponentChange();  
-  }
-
-  virtual void checkUpdates() override {
-    ComponentWindow::checkUpdates();
-
-    checkComponentChange();
-  }
-
-  void checkComponentChange() {
-    auto currentComponent{m_registry.getComponent<CompType>(m_currentEntity)};
-    if (currentComponent != m_component) {
-      auto oldComponent = m_component;
-      m_component = currentComponent;
-      onComponentChange(oldComponent);
+        checkComponentChange();
     }
-  }
 
-  virtual void onComponentChange(std::shared_ptr<CompType> oldComponent) {}
+    virtual void checkUpdates() override
+    {
+        ComponentWindow::checkUpdates();
+
+        checkComponentChange();
+    }
+
+    void checkComponentChange()
+    {
+        auto currentComponent{m_registry.getComponent<CompType>(m_currentEntity)};
+        if (currentComponent != m_component)
+        {
+            auto oldComponent = m_component;
+            m_component = currentComponent;
+            onComponentChange(oldComponent);
+        }
+    }
+
+    virtual void onComponentChange(std::shared_ptr<CompType> oldComponent) {}
 };
 
-}
+} // namespace UICreation
 
 #endif
